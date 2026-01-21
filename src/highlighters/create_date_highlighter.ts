4122607c452f1eddcc27b0c7f343168bb7ad294b
rameel <rameel-b@hotmail.com>
2026-01-21 19:24:15 +0500

    Use cyan instead of red for date highlighting

--- src/highlighters/create_date_highlighter.ts
+++ src/highlighters/create_date_highlighter.ts
@@ -18,6 +18,6 @@ export function create_date_highlighter(apply?: (m: string) => string): Highligh
     return {
         //        [=====================YYYY/mm/dd====================] [================================dd/mm/YYYY===============================]
         regex: /\b(?:19\d{2}|20\d{2})(?:[/-](?:0[1-9]|[12]\d|3[01])){2}|(?:0[1-9]|[12]\d|3[01])[/.-](?:0[1-9]|[12]\d|3[01])[/.-](?:19\d{2}|20\d{2})\b/g,
-        apply: apply ?? (s => s.replace(/\d+/g, pc.red))
+        apply: apply ?? (s => s.replace(/\d+/g, pc.cyan))
     };
 }
